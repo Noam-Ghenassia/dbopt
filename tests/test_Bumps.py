c23ebcd6dc47afe7c797a7294577cf396ddaa4f8
Noam Ghenassia <noam@ghenassia.org>
2022-04-05 19:02:30 +0200

    tests for updated bumps + add flax to cfg

--- tests/test_Bumps.py
+++ tests/test_Bumps.py
@@ -10,8 +10,8 @@ __license__ = "MIT"
 def test_level():
     bumps = Bumps()
     x = jnp.array([[-1, 0], [1, 0], [0, 0], [0, 1]])
-    y = jnp.array([jnp.exp(-6)+1, jnp.exp(-6)+1, 2*jnp.exp(-1.5), 2*jnp.exp(-3)])
+    y = jnp.array([jnp.exp(-6)+1, jnp.exp(-6)+1, 2*jnp.exp(-1.5), 2*jnp.exp(-3)]) - 0.75
     assert jnp.all(jnp.equal(bumps.level(x), y))
 
-    assert jnp.all(jnp.equal(bumps.level(x, 1), y-1))
+    assert jnp.all(jnp.equal(bumps.level(x, 1), y-0.25))
     
