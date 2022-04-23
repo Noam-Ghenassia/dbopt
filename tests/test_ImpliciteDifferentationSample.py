c03789a8987a290f93436e709b40b12c7fcd83c8
raphaelreinauer <reinauerr@googlemail.com>
2022-04-23 12:23:20 +0200

    fixed test

--- tests/test_ImpliciteDifferentationSample.py
+++ tests/test_ImpliciteDifferentationSample.py
@@ -1,4 +1,4 @@
-from dbopt.ImpliciteDifferentationSample import ImpliciteDifferentationSample
+from dbopt.ImpliciteDifferentiationSample import ImpliciteDifferentationSample
 
 from jax import grad
 import jax.numpy as jnp
@@ -7,10 +7,9 @@ from jaxopt.implicit_diff import custom_root
 def test_implicite_differentiation():
     t = jnp.array([1., 2., 3.])
     ids = ImpliciteDifferentationSample()
-    grads = grad(lambda t: 
-                custom_root(ids._optimality_condition)
-                (ids._inner_problem)(None, t).sum()
-            )(t)
+
+    sum = lambda a: jnp.sum(a)
+
+    grads = grad(lambda t: sum(ids.x_star(t)))(t)
     assert jnp.allclose(grads,
                         jnp.array([1., 1., 1.]))
-# %%
